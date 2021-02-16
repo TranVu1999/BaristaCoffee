0b3a975aef01c8768b149aa4c7d2c276865de847
Trần Vũ <43136546+TranVu1999@users.noreply.github.com>
2021-02-16 08:38:22 +0700

    update task_id: CLI_17

--- src/pages/Home/SmallSlider/index.js
+++ src/pages/Home/SmallSlider/index.js
@@ -2,7 +2,24 @@ import React, { Component } from 'react';
 import './style.scss';
 
 export default class SmallSlider extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            currentSlide: 1,
+            distance: 355
+        }
+    }
+
+    handleChooseSlide = (indexSlide) =>{
+        this.setState({
+            currentSlide: indexSlide
+        })
+    }
+
     render() {
+        const {currentSlide, distance} = this.state;
+
         return (
             <div className="section-padding small-slider">
                 <div className="cf-container d-flex-start small-slider__content">
@@ -17,14 +34,35 @@ export default class SmallSlider extends Component {
                     <div className="small-slider--right">
                         <div className="carousel small-carousel">
                             <div className="carousel__content small-carousel__content">
-                            <div className="item">
-                                <div className="item__content" style={{backgroundImage: 'url(https://res.cloudinary.com/doem0ysxl/image/upload/v1611851628/BaristaCoffee/other/small-slider1_girfoi.jpg'}} />
-                            </div>
+                                <div 
+                                    className="item"
+                                    style = {{
+                                        marginTop: -currentSlide * distance + "px"
+                                    }}
+                                >
+                                    <div className="item__content" style={{backgroundImage: 'url(https://res.cloudinary.com/doem0ysxl/image/upload/v1611851628/BaristaCoffee/other/small-slider1_girfoi.jpg'}} />
+                                </div>
+                                <div className="item">
+                                    <div className="item__content" style={{backgroundImage: 'url(https://res.cloudinary.com/doem0ysxl/image/upload/v1611851628/BaristaCoffee/other/small-slider1_girfoi.jpg'}} />
+                                </div>
+                                <div className="item">
+                                    <div className="item__content" style={{backgroundImage: 'url(https://res.cloudinary.com/doem0ysxl/image/upload/v1611851628/BaristaCoffee/other/small-slider1_girfoi.jpg'}} />
+                                </div>
                             </div>
                             <div className="small-carousel__dots">
-                                <button className="dot active" />
-                                <button className="dot" />
-                                <button className="dot" />
+                                <button 
+                                    className = {currentSlide === 0 ? "dot active" : "dot"} 
+                                    onClick = {() =>{this.handleChooseSlide(0)}}
+                                />
+                                <button 
+                                    className = {currentSlide === 1 ? "dot active" : "dot"} 
+                                    onClick = {() =>{this.handleChooseSlide(1)}}
+                                />
+                                <button 
+                                    className = {currentSlide === 2 ? "dot active" : "dot"} 
+                                    onClick = {() =>{this.handleChooseSlide(2)}}
+
+                                />
                             </div>
                         </div>
                     </div>
