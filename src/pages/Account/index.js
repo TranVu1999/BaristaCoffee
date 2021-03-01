0a746c8ff671aabd5bff78b104e6f38489b4636f
Trần Vũ <43136546+TranVu1999@users.noreply.github.com>
2021-03-01 15:20:21 +0700

    complete task_id: CLI_51

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './style.scss';
+
+import Breadcrumb from '../../commons/components/Breadcrumb'
+import MainPage from '../../commons/components/MainPage'
+import AccountSidebar from './AccountSidebar'
 
 export default class AccountPage extends Component {
     render() {
         return (
-            <div>
-                about page
-            </div>
+            <MainPage>
+                <Breadcrumb mainTitle = "My Account"/>
+                
+                <div className = "cf-container">
+                    <AccountSidebar/>
+                </div>
+
+            </MainPage>
         )
     }
 }
