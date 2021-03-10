a384224c1eba6b0e3745b1031f5a5d44971eda07
Trần Vũ <43136546+TranVu1999@users.noreply.github.com>
2021-03-10 17:47:10 +0700

    complete task_id: CLI_60

--- src/pages/Checkout/ConfirmInfo/index.js
+++ src/pages/Checkout/ConfirmInfo/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './style.scss';
 import api from './../../../api';
-import AccordingToggle from '../../../commons/components/AccordingToggle';
-import Popup from '../../../commons/components/Popup';
+
 
 
 class ConfirmInfo extends Component {
@@ -57,215 +56,107 @@ class ConfirmInfo extends Component {
             })
         }
 
-    }
+        this.props.onHandleChange(event);
 
-    onCheckDiscountCode = (event) =>{
-        event.preventDefault();
-        const {code} = this.state.discount;
-        const {subTotalCart} = this.props;
-        const accountInfo = JSON.parse(localStorage.getItem("accountInfo"));
-        if(accountInfo){
-            api.get(`/account/get-discount/${accountInfo.accountId}/${code}`)
-            .then(res =>{
-                if(res.data){
-                    if(subTotalCart < res.data.cost){
-                        this.setState({
-                            ...this.state,
-                            discount: {
-                                error: "The value of this code is greater than the value of the invoice.",
-                                code: "",
-                                cost: 0
-                            }
-                        })
-                            
-                    }else{
-                        this.props.onHandleGetDiscount(res.data.cost);
-                        this.setState({
-                            discount: {
-                                error: "",
-                                ...res.data
-                            },
-                            userInfo: {
-                                ...this.state.userInfo,
-                                userCode: res.data.code
-                            }
-                        })
-                    }
-                    
-                }else{
-                    this.setState({
-                        ...this.state,
-                        discount: {
-                            error: "This code is not used.",
-                            code: "",
-                            cost: 0
-                        }
-                    })
-                }
-                
-            })
-            .catch(err =>{
-                console.log("err", err)
-            })
-        }
-        
-    }
-
-    onHandleOpenLoginForm = (isOpen) =>{
-        this.setState({
-            ...this.state,
-            discount: {
-                error: "",
-                code: "",
-                cost: 0
-            }
-        })
     }
 
     render() {
-        const {userInfo, discount} = this.state;
-        const address = this.renderUserAddress (userInfo.address)
+        const {userInfo} = this.props;
+        const {address} = userInfo;
 
         return (
-            <>
-                <AccordingToggle>
-                    <div class="accordition-toggle">
-                        <input type="checkbox" id="1"/>
-                        <div class="accordition-toggle--box">
-                            <div class="accordition-span">
-                                Have a coupon? 
-                                <label for="1"> Click here to enter your code</label>
-                            </div>
-                            <div class="accordition-detail">
-                                <span>If you have a coupon code, please apply it below.</span>
-                                <form 
-                                    onSubmit = {this.onCheckDiscountCode}
-                                >
-                                    <div class="form-group">
-                                        <input 
-                                            type="text" 
-                                            class="input-control" 
-                                            placeholder="Coupon Code"
-                                            name="code"
-                                            onChange={this.onHandleChange}
-                                        />
-                                    </div>
-                                    <div class="form-group">
-                                        <button class="coffee-btn">Apply Coupon</button>
-                                    </div>
-                                </form>
-                            </div>
+
+            <div className="confirm-info">
+                <h3>BilLing &amp; Shipping</h3>
+                <form className="form-info-invoice">
+                    <div className="row">
+                        <div className="form-group">
+                            <input 
+                                type="text" 
+                                className="input-control" 
+                                placeholder="Full Name" 
+                                value = {userInfo.fullname}
+                                name = "fullname"
+                                onChange = {this.onHandleChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input 
+                                type="text" 
+                                className="input-control" 
+                                placeholder="PostCode / Zip" 
+                                value = {userInfo.userCode}
+                            />
                         </div>
                     </div>
-                </AccordingToggle>
+                    <div className="row">
+                        <div className="form-group">
+                            <input 
+                                type="text" 
+                                className="input-control" 
+                                placeholder="Province / City" 
+                                value = {address.province}
+                                name = "province"
+                                onChange = {this.onHandleChange}
 
-                <div className="confirm-info">
-                    <h3>BilLing &amp; Shipping</h3>
-                    <form className="form-info-invoice">
-                        <div className="row">
-                            <div className="form-group">
-                                <input 
-                                    type="text" 
-                                    className="input-control" 
-                                    placeholder="Full Name" 
-                                    value = {userInfo.fullname}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <input 
-                                    type="text" 
-                                    className="input-control" 
-                                    placeholder="PostCode / Zip" 
-                                    value = {userInfo.userCode}
-                                />
-                            </div>
+                            />
                         </div>
-                        <div className="row">
-                            <div className="form-group">
-                                <input 
-                                    type="text" 
-                                    className="input-control" 
-                                    placeholder="Province / City" 
-                                    value = {address.province}
-
-                                />
-                            </div>
-                            <div className="form-group">
-                                <input 
-                                    type="text" 
-                                    className="input-control" 
-                                    placeholder="District"
-                                    value = {address.district}
-                                />
-                            </div>
+                        <div className="form-group">
+                            <input 
+                                type="text" 
+                                className="input-control" 
+                                placeholder="District"
+                                value = {address.district}
+                                name = "district"
+                                onChange = {this.onHandleChange}
+                            />
                         </div>
-                        <div className="row">
-                            <div className="form-group">
-                                <input
-                                    type="text" 
-                                    className="input-control" 
-                                    placeholder="Wards" 
-                                    value = {address.wards}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <input 
-                                    type="text" 
-                                    className="input-control" 
-                                    placeholder="House number and street name" 
-                                    value = {address.houseNumber}
-                                />
-                            </div>
+                    </div>
+                    <div className="row">
+                        <div className="form-group">
+                            <input
+                                type="text" 
+                                className="input-control" 
+                                placeholder="Wards" 
+                                value = {address.wards}
+                                name = "wards"
+                                onChange = {this.onHandleChange}
+                            />
                         </div>
-                        <div className="row">
-                            <div className="form-group">
-                                <input 
-                                    type="text" 
-                                    className="input-control" 
-                                    placeholder="Phone" 
-                                    value = {address.phoneNumber}
-                                />
-                            </div>
-                            <div className="form-group">
-                                <input 
-                                    type="text" 
-                                    className="input-control" 
-                                    placeholder="Email" 
-                                    value = {userInfo.email}
-                                />
-                            </div>
+                        <div className="form-group">
+                            <input 
+                                type="text" 
+                                className="input-control" 
+                                placeholder="House number and street name" 
+                                value = {address.houseNumber}
+                                name = "houseNumber"
+                                onChange = {this.onHandleChange}
+                            />
                         </div>
-                    </form>
-                </div>
-
-                <Popup 
-                    popupTitle="Notify" isOpen = {discount.error !== ''}
-                    onHandleOpenLoginForm = {this.onHandleOpenLoginForm}
-                >
-                    <AccordingToggle>
-                        <div className="accordition-toggle--box">
-                            <div className = "accordition-span">{discount.error}</div>
+                    </div>
+                    <div className="row">
+                        <div className="form-group">
+                            <input 
+                                type="text" 
+                                className="input-control" 
+                                placeholder="Phone" 
+                                value = {address.phoneNumber}
+                                name = "phoneNumber"
+                                onChange = {this.onHandleChange}
+                            />
                         </div>
-                    </AccordingToggle>
-                </Popup>
-            </>
+                        <div className="form-group">
+                            <input 
+                                type="text" 
+                                className="input-control" 
+                                placeholder="Email" 
+                                value = {userInfo.email}
+                            />
+                        </div>
+                    </div>
+                </form>
+            </div>
         )
-    }
-
-    componentDidMount(){
-        const accountInfo = JSON.parse(localStorage.getItem("accountInfo"));
-        if(accountInfo){
-            api.get(`account/get-user/${accountInfo.accountId}`)
-            .then(res =>{
-                this.setState({
-                    ...this.setState,
-                    userInfo: res.data
-                })
-            })
-            .catch(err =>{
-                console.log("err", err)
-            })
-        }
     }
 }
 
--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -4,7 +4,11 @@ import Breadcrumb from './../../commons/components/Breadcrumb';
 import AccordingToggle from '../../commons/components/AccordingToggle';
 import ConfirmInfo from './ConfirmInfo';
 import ConfirmInvoice from './ConfirmInvoice';
+import Popup from '../../commons/components/Popup';
 
+import {NavLink} from 'react-router-dom';
+
+import api from './../../api';
 import {connect} from 'react-redux';
 import {actUpdateUrl} from './../../commons/modules/Url/actions';
 
@@ -12,8 +16,114 @@ class CheckoutPage extends Component {
     constructor(props){
         super(props);
         this.state = {
-            discountCost: 0
+            discountCost: 0,
+            submitted: false,
+            discount: {
+                error: "",
+                code: "",
+                cost: 0
+            },
+            userInfo: {
+                userCode: "",
+                email: "",
+                phoneNumber: "",
+                fullname: "",
+                address: {
+                    company: "",
+                    province: "",
+                    district: "",
+                    wards: "",
+                    houseNumber: "",
+                    phoneNumber:""
+                }
+            }
         }
+    }
+
+    onHandleChange = (event) =>{
+        const {name, value} = event.target;
+        
+        if(name === "code"){
+            this.setState({
+                ...this.state,
+                discount: {
+                    ...this.state.discount,
+                    code: value
+                }
+            })
+        }else if(name === "fullname"){
+            this.setState({
+                ...this.state,
+                userInfo: {
+                    ...this.state.userInfo,
+                    [name]: value
+                }
+            })
+        }else{
+            this.setState({
+                ...this.state,
+                userInfo: {
+                    ...this.state.userInfo,
+                    address: {
+                        ...this.state.userInfo.address,
+                        [name]: value
+                    }
+                }
+            })
+        }
+
+    }
+
+    onCheckDiscountCode = (event) =>{
+        event.preventDefault();
+        const {code} = this.state.discount;
+        const subTotalCart = this.getSubTotalCart();
+
+        const accountInfo = JSON.parse(localStorage.getItem("accountInfo"));
+        if(accountInfo){
+            api.get(`/account/get-discount/${accountInfo.accountId}/${code}`)
+            .then(res =>{
+                if(res.data){
+                    if(subTotalCart < res.data.cost){
+                        this.setState({
+                            ...this.state,
+                            discount: {
+                                error: "The value of this code is greater than the value of the invoice.",
+                                code: "",
+                                cost: 0
+                            }
+                        })
+                            
+                    }else{
+                        this.setState({
+                            discount: {
+                                error: "",
+                                ...res.data
+                            },
+                            userInfo: {
+                                ...this.state.userInfo,
+                                userCode: res.data.code
+                            }
+                        })
+                    }
+                    
+                }else{
+                    this.setState({
+                        ...this.state,
+                        discount: {
+                            error: "This code is not used.",
+                            code: "",
+                            cost: 0
+                        }
+                    })
+                }
+                
+            })
+            .catch(err =>{
+                console.log("err", err)
+            })
+        }
+        
     }
 
     onHandleGetDiscount = (cost) =>{
@@ -31,42 +141,183 @@ class CheckoutPage extends Component {
         return cost;
     }
 
+    onHandleOpenLoginForm = (isOpen) =>{
+        this.setState({
+            ...this.state,
+            submitted: false,
+            discount: {
+                error: "",
+                code: "",
+                cost: 0
+            }
+        })
+    }
+
+    onSubmitInvoice = () =>{
+        const accountInfo = JSON.parse(localStorage.getItem("accountInfo"));
+        if(accountInfo){
+            const {discount, userInfo} = this.state;
+            const total = this.getSubTotalCart();
+
+            const listProduct = this.props.dataCart.map(item =>{
+                return {
+                    productId: item.prodId,
+                    amount: item.amount
+                }
+            })
+
+            const data = {
+                discount: discount.code ? discount.code : "none",
+                accountId: accountInfo.accountId,
+                total: total - discount.cost,
+                address: `${userInfo.address.houseNumber}, ${userInfo.address.wards}, ${userInfo.address.district}, ${userInfo.address.province}`,
+                receivedName: userInfo.fullname, 
+                phone: userInfo.address.phoneNumber,
+                listProduct
+            }
+
+            api.post(`http://localhost:9000/invoice/submit`, data)
+            .then(res =>{
+                if(res.data){
+                    this.setState({
+                        ...this.state,
+                        submitted: true
+                    })
+                }
+                
+            })
+            .catch(err =>{
+                console.log("err", err)
+            })
+        }
+    }
+
     render() {
         const {dataCart} = this.props;
         const subTotalCart = this.getSubTotalCart();
-        const {discountCost} = this.state;
+        const {discountCost, userInfo, discount, submitted} = this.state;
 
         return (
-            <MainPage>
-                <Breadcrumb mainTitle = "Checkout"/>
-                
-                <div class="cf-container checkout-page">
+            <>
+                <MainPage>
+                    <Breadcrumb mainTitle = "Checkout"/>
+                    
+                    <div class="cf-container checkout-page">
+                        <AccordingToggle>
+                            <div class="accordition-toggle">
+                                <input type="checkbox" id="1"/>
+                                <div class="accordition-toggle--box">
+                                    <div class="accordition-span">
+                                        Have a coupon? 
+                                        <label for="1"> Click here to enter your code</label>
+                                    </div>
+                                    <div class="accordition-detail">
+                                        <span>If you have a coupon code, please apply it below.</span>
+                                        <form 
+                                            onSubmit = {this.onCheckDiscountCode}
+                                        >
+                                            <div class="form-group">
+                                                <input 
+                                                    type="text" 
+                                                    class="input-control" 
+                                                    placeholder="Coupon Code"
+                                                    name="code"
+                                                    value={discount.code}
+                                                    onChange={this.onHandleChange}
+                                                />
+                                            </div>
+                                            <div class="form-group">
+                                                <button class="coffee-btn">Apply Coupon</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </AccordingToggle>
 
-                    <ConfirmInfo 
-                        subTotalCart = {subTotalCart}
-                        onHandleGetDiscount = {this.onHandleGetDiscount}
-                    />
-                    <ConfirmInvoice 
-                        dataCart= {dataCart}
-                        subTotalCart = {subTotalCart}
-                        discountCost = {discountCost}
-                    />
-                    <div class="confirm-action">
-                        <button class="coffee-btn">Place Order</button>
+                        <ConfirmInfo 
+                            onHandleGetDiscount = {this.onHandleGetDiscount}
+                            onHandleChange = {this.onHandleChange}
+                            userInfo = {userInfo}
+                        />
+                        <ConfirmInvoice 
+                            dataCart= {dataCart}
+                            subTotalCart = {subTotalCart}
+                            discountCost = {discountCost}
+                        />
+                        <div class="confirm-action">
+                            {dataCart.length > 0 ? (
+                                <button 
+                                    class="coffee-btn"
+                                    onClick = {this.onSubmitInvoice}
+                                >Place Order</button>
+                            ):(
+                                <NavLink 
+                                    className="return-to-shop" 
+                                    to="/shop"
+                                >Return to shop</NavLink>
+                            )}
+                            
+                            
+                        </div>
                     </div>
-                </div>
-                
-                
-            </MainPage>
+                    
+                    
+                </MainPage>
+                <Popup 
+                    popupTitle="Notify" isOpen = {discount.error !== '' || submitted}
+                    onHandleOpenLoginForm = {this.onHandleOpenLoginForm}
+                >
+                    <AccordingToggle>
+                        <div className="accordition-toggle--box">
+                            <div className = "accordition-span">{
+                               submitted ? "Your invoice is submitted." : discount.error
+                            }</div>
+                        </div>
+                    </AccordingToggle>
+                </Popup>
+            </>
         )
     }
 
     componentDidMount(){
+        
         this.props.onUpdateUrl({
             params: this.props.match.params,
             url: this.props.match.url,
             path: this.props.match.path
         })
+
+        const accountInfo = JSON.parse(localStorage.getItem("accountInfo"));
+        if(accountInfo){
+            api.get(`account/get-user/${accountInfo.accountId}`)
+            .then(res =>{
+                let address = {
+                    company: "",
+                    province: "",
+                    district: "",
+                    wards: "",
+                    houseNumber: "",
+                    phoneNumber:""
+                };
+                for(let addressItem of res.data.address){
+                    if(addressItem.isDefault){
+                        address = {...addressItem};
+                        break;
+                    }
+                }
+                this.setState({
+                    ...this.setState,
+                    userInfo: {
+                        ...res.data,
+                        address
+                    }
+                })
+            })
+            .catch(err =>{
+                console.log("err", err)
+            })
+        }
     }
 }
 
