f59010a310afa2b1a05e0ddde5479812f938cf6d
Trần Vũ <43136546+TranVu1999@users.noreply.github.com>
2021-03-23 00:21:17 +0700

    update cart

--- src/commons/components/Cart/index.js
+++ src/commons/components/Cart/index.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import UpdateCart from '../UpdateCart';
 import './style.scss';
 
 export default class Cart extends Component {
 
     render() {
         return (
-            <div className="cart">
+            <div className="cart open">
                 <div className="cart-container">
                     <div className = "cart--title">
                         <h2>You are purchasing</h2>
                         <button><span aria-hidden="true" className="icon_close"></span></button>
                     </div>
                     <div className = "cart--body">
+                        <div className = "cart--item">
+                            <div className = "product-thumb">
+                                <img src ="https://res.cloudinary.com/doem0ysxl/image/upload/v1611851631/BaristaCoffee/shop/prod6_epuaqx.jpg" alt="product"/>
+                            </div>
+                            <div className = "product-text">
+                                <button className = "btn-close"><span aria-hidden="true" className="icon_close"></span></button>
+                                <h4>Coffee Pot</h4>
+                                <p className = "product-price">$29.00</p>
+                                <div className = "update-cart">
+                                    <button className="btn-increase">
+                                        <span aria-hidden="true" className="icon_minus-06" />
+                                    </button>
+                                    <input type="text" defaultValue="1"/>
+                                    <button className="btn-decrease"><span aria-hidden="true" className="icon_plus"/></button>
+                                </div>
+                            </div>
+                        </div>
                         <div className = "cart--item">
                             <div className = "product-thumb">
                                 <img src ="https://res.cloudinary.com/doem0ysxl/image/upload/v1611851631/BaristaCoffee/shop/prod6_epuaqx.jpg" alt="product"/>
